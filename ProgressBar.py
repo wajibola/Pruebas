3797896c7450cb947c0f578cc6e7cd3ee2a298bd
wajibola <waziri.ajibola.lawal@gmail.com>
2020-03-22 17:20:33 +0100

    Implementation of progress bar start button command method.

--- ProgressBar.py
+++ ProgressBar.py
@@ -17,11 +17,31 @@ class ProgressBarApp(Tk):
                                         length=200, mode="determinate")
         self.progress_bar.pack(side=TOP)
     
-        self.btn_start = Button(text="Start")
+        self.btn_start = Button(text="Start", command = self.start_progress_bar)
         self.btn_start.pack(side=LEFT)
         
         self.btn_stop = Button(text="Stop")
         self.btn_stop.pack(side=LEFT)
+        
+        self.barVal = 0
+        self.maxbarVal = 5000
+        self.enable_bar = False
+        
+    def update_bar(self):
+        if self.enable_bar == True:
+            self.barVal += 1000
+            self.progress_bar["value"] = self.barVal
+            if (self.barVal < self.maxbarVal):
+                # call update_bar every 1000 ms
+                self.after(1000, self.update_bar)
+            else:
+                print("Progress bar updat completed")
+
+    def start_progress_bar(self):
+        self.enable_bar = True
+        self.progress_bar["value"] = 0
+        self.progress_bar["maximum"] = 5000
+        self.update_bar()
     
     
 pBar = ProgressBarApp()
