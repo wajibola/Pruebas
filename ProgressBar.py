6eac24a9c2cba55ce55ba9159ecab818a09119ed
wajibola <waziri.ajibola.lawal@gmail.com>
2020-03-22 13:36:17 +0100

    Creation of first draft in develop branch. Creating class ProgressBar.

---
+++ ProgressBar.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 15:32:33 2020
+
+@author: waziri
+"""
+
+from tkinter import *
+from tkinter.ttk import *
+
+class ProgressBarApp(Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self,*args, **kwargs)
+        
+
+
